97f1cc4c4c725723adb791dddec7c0c2578c64ea
neenza13 <123584087+neenza13@users.noreply.github.com>
2023-02-17 21:37:34 -0600

    Pruebastexto

---
+++ pruebasconvisualstudio.kt
@@ -0,0 +1,22 @@
+
+
+
+class usuariosycontrasenas {
+
+    fun users(){
+        var usuario: Int = 20
+        var contra = "Victor"
+
+        var noempleado: Int = 20
+        var contrasena = "Victor"
+
+        if (usuario == noempleado){
+            if (contra == contrasena){
+                println ("El usuario es correcto")
+
+            }
+        }
+        
+    }
+
+}
